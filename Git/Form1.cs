98fe6601ded3c32d990ec5ddb46be1ad5427aa2c
Zaden Swain <mrlazertagman22@gmail.com>
2021-10-21 08:15:53 -0500

    second commit

--- Git/Form1.cs
+++ Git/Form1.cs
@@ -19,7 +19,7 @@ namespace Git
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Git that");
+            MessageBox.Show("Git that hub");
 
         }
     }
